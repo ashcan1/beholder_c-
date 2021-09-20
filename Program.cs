cc72d843594471c716fa57c5b958bf01a6d97049
ashcan1 <ashki444@gmail.com>
2021-09-20 19:21:45 +0100

    d

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;  
+public class Exercise66
+{  
+    public static void Main()
+    {
+        int row,column; 
+
+ 
+        for(row=0;row<=6;row++)
+        {
+            for (column=0; column<=6; column++)
+            {
+                if (column == 3|| (row == 0 && column > 0 && column < 6) || (row == 6 && column > 0 && column < 6))
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+                
+           
+            
+            Console.Write("\n");
+        }
+        Console.Write("\n");
+    }
+}
