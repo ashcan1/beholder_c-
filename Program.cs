d7d7b965a020b4f56764f2fcae497ec395a2bf94
ashcan1 <ashki444@gmail.com>
2021-09-20 20:00:42 +0100

    num

--- Program.cs
+++ Program.cs
@@ -3,14 +3,16 @@ public class Exercise66
 {  
     public static void Main()
     {
+        Console.Write("Enter the number of rows:\t");
+        int num = int.Parse(Console.ReadLine());
         int row,column; 
 
  
-        for(row=0;row<=6;row++)
+        for(row=0;row<=num;row++)
         {
-            for (column=0; column<=6; column++)
+            for (column=0; column<=num; column++)
             {
-                if (column == 3|| (row == 0 && column > 0 && column < 6) || (row == 6 && column > 0 && column < 6))
+                if (column == num|| (row == 0 && column > 0 && column < num) || (row == num && column > 0 && column < num))
                     Console.Write("*");
                 else
                     Console.Write(" ");
