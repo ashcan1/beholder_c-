1695c7f047e43e8e52cc1f0fd5bb13310d432c3d
ashcan1 <ashki444@gmail.com>
2021-09-21 20:08:59 +0100

    mathround

--- Program.cs
+++ Program.cs
@@ -7,24 +7,31 @@ public class Exercise66
         Console.Write("Enter the number of rows:\t");
         int num = int.Parse(Console.ReadLine());
 
-        int t, m, b;
+        int t, m, b, s ;
         for (t = 1; t <= num; ++t)
         {
             Console.Write("* ");
         }
         Console.WriteLine();
-        
-        for (m = 1; m <= num; ++m )
+
+
+
+        for (m = 1; m <= num / 2; ++m)
+
         {
-            Console.Write("* *");
+            for (s = 1; s <= num; s++)
+            {
+                Console.Write(" ");
+            }
+
+            for (s = 1; s <= m; ++s)
+            {
+                Console.Write("*");
+            }
+
             Console.WriteLine();
-         
-            
-            
         }
-
-
-        Console.WriteLine();
+        
         for (b = 1; b <= num; ++b)
         {
             Console.Write("* ");
