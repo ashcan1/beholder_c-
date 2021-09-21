84e8edf51585bb3b2ec41e8857fba2435a2bd586
ashcan1 <ashki444@gmail.com>
2021-09-21 15:54:14 +0100

    d

--- Program.cs
+++ Program.cs
@@ -1,27 +1,34 @@
-﻿using System;  
+﻿using System;
+
 public class Exercise66
-{  
+{
     public static void Main()
     {
         Console.Write("Enter the number of rows:\t");
         int num = int.Parse(Console.ReadLine());
-        int row,column; 
 
- 
-        for(row=0;row<=num;row++)
+        int t, m, b;
+        for (t = 1; t <= num; ++t)
         {
-            for (column=0; column<=num; column++)
-            {
-                if (column == num|| (row == 0 && column > 0 && column < num) || (row == num && column > 0 && column < num))
-                    Console.Write("*");
-                else
-                    Console.Write(" ");
-            }
-                
-           
-            
-            Console.Write("\n");
+            Console.Write("* ");
         }
-        Console.Write("\n");
+        Console.WriteLine();
+        
+        for (m = 1; m <= num; ++m )
+        {
+            Console.Write("* *");
+            Console.WriteLine();
+         
+            
+            
+        }
+
+
+        Console.WriteLine();
+        for (b = 1; b <= num; ++b)
+        {
+            Console.Write("* ");
+        }
     }
 }
+     
