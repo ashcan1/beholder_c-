24970dde0d79f30e0bfed4b1ea4bbda6615327f3
ashcan1 <ashki444@gmail.com>
2021-09-22 15:55:29 +0100

    odd

--- Program.cs
+++ Program.cs
@@ -7,50 +7,41 @@ public class Exercise66
         Console.Write("Enter the number of rows:\t");
         int num = int.Parse(Console.ReadLine());
 
-        int t, m, b, s, e;
-        for (t = 1; t <= num; ++t)
-        {
-            Console.Write("* ");
+        int t,b;
+        
+        for (t = 1; t <= num; t++)
+        { Console.Write("*");
         }
         Console.WriteLine();
 
 
 
-        for (m = 1; m <= num / 2; ++m)
+        for (int i = 1; i <= num - 2; i++)
         {
-      
-
-            if (num % 2 == 0)
-            { 
-                Console.WriteLine("* ");
-            }
-            else
+            bool isEven = num % 2 == 0;
+            int starCount = isEven ? 2 : 1;
+            
+            for (int s = 1; s <= num - s; ++s)
             {
-                {
-                    for (s = 1; s <= num; s++)
-                    {
-                        Console.Write(" ");
-                    }
-
-                    for (s = 1; s <= m; ++s)
-                    {
-                        Console.Write("*");
-                    }
-                }
+                Console.Write(" ");
             }
 
 
-
-
-
-
+            for (int j = 1; j <= starCount; j++)
+            {
+                Console.Write("*");
+        
+            }
+            
 
             Console.WriteLine();
         }
         
-        for (b = 1; b <= num; ++b)
+        
+        
+        for (b = 1; b <= num; b++)
         {
-            Console.Write("* ");
+            Console.Write("*");
         }
     }
 }
